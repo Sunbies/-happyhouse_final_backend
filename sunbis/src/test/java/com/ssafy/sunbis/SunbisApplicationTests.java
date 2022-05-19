feec7e03361a5db9c0d67e55b21f72ccc7d4320b
dladndrbs <dladndrbs@naver.com>
2022-05-19 18:18:40 +0900

    백엔드 첫 세팅

---
+++ sunbis/src/test/java/com/ssafy/sunbis/SunbisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.sunbis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunbisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
